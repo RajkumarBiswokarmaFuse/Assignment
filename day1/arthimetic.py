81052b176ad9f49e77752b3cd6286fdf7ace3769
RajkumarBiswokarmaFuse <raj.biswokarma@fusemachines.com>
2023-08-02 12:02:02 +0545

    feat: function for arthimetic calculation

---
+++ day1/arthimetic.py
@@ -0,0 +1,11 @@
+def addition(num1 , num2):
+    """ This function takes an input of two numbers and returns its sum"""
+    return num1 + num2
+
+def subtraction(num1, num2):
+    """ This function takes an input of two numbers and returns its difference """
+    return num1-num2
+
+def multiplication(num1, num2):
+    """This function takes an input of two numbers and returns its product"""
+    return num1 * num2
